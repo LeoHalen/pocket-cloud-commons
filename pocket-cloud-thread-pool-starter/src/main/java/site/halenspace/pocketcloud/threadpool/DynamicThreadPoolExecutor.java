eb21a8af10cd6aaf3468235ad674389790d5a8b1
halen <halenmail@126.com>
2021-06-27 23:59:53 +0800

    feat(动态线程池): 初始化动态线程池组件工程

---
+++ pocket-cloud-thread-pool-starter/src/main/java/site/halenspace/pocketcloud/threadpool/DynamicThreadPoolExecutor.java
@@ -0,0 +1,29 @@
+package site.halenspace.pocketcloud.threadpool;
+
+import java.util.concurrent.*;
+
+/**
+ * 支持动态设置参数的线程池
+ *
+ * @Author Halen.Leo · 2021/6/27
+ * @Blogger 后起小生
+ * @Github https://github.com/LeoHalen
+ */
+public class DynamicThreadPoolExecutor extends ThreadPoolExecutor {
+
+    public DynamicThreadPoolExecutor(int corePoolSize, int maximumPoolSize, long keepAliveTime, TimeUnit unit, BlockingQueue<Runnable> workQueue) {
+        super(corePoolSize, maximumPoolSize, keepAliveTime, unit, workQueue);
+    }
+
+    public DynamicThreadPoolExecutor(int corePoolSize, int maximumPoolSize, long keepAliveTime, TimeUnit unit, BlockingQueue<Runnable> workQueue, ThreadFactory threadFactory) {
+        super(corePoolSize, maximumPoolSize, keepAliveTime, unit, workQueue, threadFactory);
+    }
+
+    public DynamicThreadPoolExecutor(int corePoolSize, int maximumPoolSize, long keepAliveTime, TimeUnit unit, BlockingQueue<Runnable> workQueue, RejectedExecutionHandler handler) {
+        super(corePoolSize, maximumPoolSize, keepAliveTime, unit, workQueue, handler);
+    }
+
+    public DynamicThreadPoolExecutor(int corePoolSize, int maximumPoolSize, long keepAliveTime, TimeUnit unit, BlockingQueue<Runnable> workQueue, ThreadFactory threadFactory, RejectedExecutionHandler handler) {
+        super(corePoolSize, maximumPoolSize, keepAliveTime, unit, workQueue, threadFactory, handler);
+    }
+}
