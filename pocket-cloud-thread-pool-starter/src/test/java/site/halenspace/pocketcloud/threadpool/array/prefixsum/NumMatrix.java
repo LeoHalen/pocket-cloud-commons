95558bf2f7c8dae107629abd8967da94821d78ef
Halen Leo <halenmail@126.com>
2022-04-29 17:24:56 +0800

    feat(算法):前缀和问题

--- pocket-cloud-thread-pool-starter/src/test/java/site/halenspace/pocketcloud/threadpool/array/prefixsum/NumMatrix.java
+++ pocket-cloud-thread-pool-starter/src/test/java/site/halenspace/pocketcloud/threadpool/array/prefixsum/NumMatrix.java
@@ -10,6 +10,8 @@ public class NumMatrix {
     // preNum[i][j] 记录着matrix子矩阵 [0, 0, i - 1, j - 1]的元素和
     private int[][] preSum;
 
+    public NumMatrix() {}
+
     public NumMatrix(int[][] matrix) {
         int m = matrix.length, n = matrix[0].length;
         if (n == 0) return;
@@ -81,8 +83,62 @@ public class NumMatrix {
         return res;
     }
 
-    public int subarraySum(int[] nums, int k) {
+    /* 时间复杂度O(n)的解法 */
+    int subarraySumTemp(int[] nums, int k) {
+        int n = nums.length;
+        // map：前缀和 -> 该前缀和出现的次数
+        HashMap<Integer, Integer>
+                preSum = new HashMap<>();
+        // base case
+        preSum.put(0, 1);
 
-        return 1;
+        int res = 0, sum0_i = 0;
+        for (int i = 0; i < n; i++) {
+            sum0_i += nums[i];
+            // 这是我们想找的前缀和 nums[0..j]
+            int sum0_j = sum0_i - k;
+            // 如果前面有这个前缀和，则直接更新答案
+            if (preSum.containsKey(sum0_j))
+                res += preSum.get(sum0_j);
+            // 把前缀和 nums[0..i] 加入并记录出现次数
+            preSum.put(sum0_i,
+                    preSum.getOrDefault(sum0_i, 0) + 1);
+        }
+        return res;
+    }
+
+    /**
+     * 560. 和为 K 的子数组
+     * 解题思路：构造前缀和，穷举所有子数组的和
+     * 时间复杂度: O(N^2)
+     * 空间复杂度: O(N)
+     * @param nums 整数数组
+     * @param k 子数组长度
+     * @return 符合的子数组个数
+     */
+    public int subarraySum(int[] nums, int k) {
+        int n = nums.length;
+        // 构造前缀和
+        int[] preNums = new int[n + 1];
+        preNums[0] = 0;
+        for (int i = 0; i < n; i++) {
+            preNums[i + 1] = preNums[i] + nums[i];
+        }
+
+        int count = 0;
+        for (int i = 1; i <= n; i++) {
+            for (int j = 0; j < i; j++) {
+                if (preNums[i] - preNums[j] == k) {
+                    count++;
+                }
+            }
+        }
+
+        return count;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = new int[]{1, 1, 1};
+        new NumMatrix().subarraySum(nums, 2);
     }
 }
