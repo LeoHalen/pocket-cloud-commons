c6c134ff0cc7b5b33695bb92eb8caf5698a70d6b
Halen.Leo <halenmail@126.com>
2022-08-31 23:32:28 +0800

    feat(动态线程池): 指标收集代码同步

---
+++ thread-pool-common/thread-pool-core/src/main/java/site/halenspace/pocket/threadpool/metrics/MetricsGroup.java
@@ -0,0 +1,21 @@
+package site.halenspace.pocket.threadpool.metrics;
+
+import com.codahale.metrics.Gauge;
+import com.codahale.metrics.MetricRegistry;
+
+/**
+ * @author Halen Leo · 2022/8/31
+ * @blogger 后起小生
+ * @github <a href="https://github.com/LeoHalen"/>
+ */
+public class MetricsGroup<U> {
+
+//    public <T extends Gauge<U>> T newGauge(String name) {
+//        MetricsManager.registry().gauge()
+//        return (T) getOrAdd(name, MetricRegistry.MetricBuilder.GAUGES);
+//    }
+//
+//    public String metricName(String group, String name, ) {
+//
+//    }
+}
--- thread-pool-common/thread-pool-core/src/main/java/site/halenspace/pocket/threadpool/metrics/MetricsManager.java
+++ thread-pool-common/thread-pool-core/src/main/java/site/halenspace/pocket/threadpool/metrics/MetricsManager.java
@@ -1,7 +1,10 @@
 package site.halenspace.pocket.threadpool.metrics;
 
+import com.codahale.metrics.ConsoleReporter;
 import com.codahale.metrics.Gauge;
 import com.codahale.metrics.MetricRegistry;
+
+import java.util.concurrent.TimeUnit;
 
 /**
  * @author Halen Leo · 2022/8/30
@@ -10,11 +13,40 @@ import com.codahale.metrics.MetricRegistry;
  */
 public class MetricsManager {
 
-    public static void main(String[] args) {
+    /* Singleton instance */
+    public static final MetricsManager INSTANCE = new MetricsManager();
+    /* Default metric registry */
+    private final MetricRegistry registry = new MetricRegistry();
+
+    private MetricsManager() {}
+
+    public static MetricRegistry registry() {
+        return INSTANCE.registry;
+    }
+
+    public void test() {
         MetricRegistry registry = new MetricRegistry();
         registry.register(
-                MetricRegistry.name("", "queue", "size"),
+                MetricRegistry.name("test", "queue", "size"),
                 (Gauge<Integer>) () -> 10
         );
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        MetricRegistry registry = new MetricRegistry();
+        registry.register(
+                MetricRegistry.name("test", "queue", "size"),
+                (Gauge<Integer>) () -> 10
+        );
+
+        Gauge<Integer> gauge = registry.gauge(MetricRegistry.name("test", "queue", "size"));
+
+        ConsoleReporter.forRegistry(registry)
+                .convertRatesTo(TimeUnit.SECONDS)
+                .convertDurationsTo(TimeUnit.MILLISECONDS)
+                .build()
+                .start(1, TimeUnit.SECONDS);
+
+        Thread.sleep(5000);
     }
 }
