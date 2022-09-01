2011f14ddb7987d969c374b6a45bfb72860fc24f
Halen Leo <halenmail@126.com>
2022-09-01 18:03:53 +0800

    feat(动态线程池): 测试代码同步

--- thread-pool-common/thread-pool-core/src/main/java/site/halenspace/pocket/threadpool/metrics/MetricsManager.java
+++ thread-pool-common/thread-pool-core/src/main/java/site/halenspace/pocket/threadpool/metrics/MetricsManager.java
@@ -3,6 +3,9 @@ package site.halenspace.pocket.threadpool.metrics;
 import com.codahale.metrics.ConsoleReporter;
 import com.codahale.metrics.Gauge;
 import com.codahale.metrics.MetricRegistry;
+import com.yammer.metrics.Metrics;
+import com.yammer.metrics.core.MetricName;
+import com.yammer.metrics.core.MetricsRegistry;
 
 import java.util.concurrent.TimeUnit;
 
@@ -24,15 +27,7 @@ public class MetricsManager {
         return INSTANCE.registry;
     }
 
-    public void test() {
-        MetricRegistry registry = new MetricRegistry();
-        registry.register(
-                MetricRegistry.name("test", "queue", "size"),
-                (Gauge<Integer>) () -> 10
-        );
-    }
-
-    public static void main(String[] args) throws InterruptedException {
+    public static void mainOld(String[] args) throws InterruptedException {
         MetricRegistry registry = new MetricRegistry();
         registry.register(
                 MetricRegistry.name("test", "queue", "size"),
@@ -40,7 +35,6 @@ public class MetricsManager {
         );
 
         Gauge<Integer> gauge = registry.gauge(MetricRegistry.name("test", "queue", "size"));
-
         ConsoleReporter.forRegistry(registry)
                 .convertRatesTo(TimeUnit.SECONDS)
                 .convertDurationsTo(TimeUnit.MILLISECONDS)
@@ -48,5 +42,34 @@ public class MetricsManager {
                 .start(1, TimeUnit.SECONDS);
 
         Thread.sleep(5000);
+    }
+    public static void main2(String[] args) throws InterruptedException {
+        MetricsRegistry registry = new MetricsRegistry();
+        registry.newGauge(
+                new MetricName("group", "type", "name", "scope", "mBeanName"),
+                new com.yammer.metrics.core.Gauge<Integer>() {
+                    @Override
+                    public Integer value() {
+                        return 10;
+                    }
+                }
+        );
+
+        com.yammer.metrics.reporting.ConsoleReporter.enable(registry, 1, TimeUnit.SECONDS);
+
+        Thread.sleep(5000);
+    }
+    public static void main(String[] args) throws InterruptedException {
+        Metrics.defaultRegistry().newGauge(
+                new MetricName("group", "type", "name", "scope", "mBeanName"),
+                new com.yammer.metrics.core.Gauge<Integer>() {
+                    @Override
+                    public Integer value() {
+                        return 10;
+                    }
+                }
+        );
+
+        Thread.sleep(60000 * 10);
     }
 }
